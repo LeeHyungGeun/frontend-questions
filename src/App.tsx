788dd896d37b9f820cbb7184bd281aad4266e79d
leehyunggeun <leehyunggeun@navercorp.com>
2018-02-17 00:52:37 +0800

    add Decorator, withLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import routes from './routes';
-import Layout from './containers/LayoutContainer';
 
 class App extends React.Component {
     render() {
         return (
-            <Layout>
-                <BrowserRouter children={routes} />
-            </Layout>
+            <BrowserRouter children={routes} />
         );
     }
 }
---
+++ src/Containers/withLayout.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Layout from './LayoutContainer';
+
+type Props = {};
+type State = {};
+function withLayout(WrappedComponent: React.ComponentType): any {
+    return class WLC extends React.Component<Props, State> {
+        constructor(props: Props) {
+            super(props);
+        }
+        render() {
+            return (
+                <Layout>
+                    <WrappedComponent {...this.props} />
+                </Layout>
+            );
+        }
+    };
+}
+
+export default withLayout;
